1eac551893e3be2255410528e4444d8b674c981d
mkhairul <mkhairul@gmail.com>
2016-02-10 22:12:12 +0800

    updates

--- public/assets/js/controllers/commission.js
+++ public/assets/js/controllers/commission.js
@@ -111,8 +111,9 @@ app.controller('CommissionController',
         });
 	}
     
-    $scope.getGB = function(user){
-      console.log(user);
+    $scope.getGB = function(level, membership){
+      console.log(level);
+      console.log(membership);
       if(level && membership)
       {
         var result = $filter('filter')($scope.commissions, { "level":level, "membership":membership }, true)[0];
