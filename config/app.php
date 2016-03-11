feb6d85d711d7848966abc38a928c7dd03236fe0
Khairul Anuar <khairul@logicalincrements.com>
2016-03-11 16:54:14 +0800

    added cors

--- config/app.php
+++ config/app.php
@@ -145,6 +145,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+      
+        'Barryvdh\Cors\ServiceProvider',
 
     ],
 
